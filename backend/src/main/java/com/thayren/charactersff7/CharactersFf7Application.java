36e46570d08ffa003e31d889311eec2c3859382e
Renan Duarte <renandg.axl@gmail.com>
2023-04-04 18:44:45 -0300

    Creating the characters-ff7 project

---
+++ backend/src/main/java/com/thayren/charactersff7/CharactersFf7Application.java
@@ -0,0 +1,13 @@
+package com.thayren.charactersff7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharactersFf7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharactersFf7Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thayren/charactersff7/CharactersFf7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thayren.charactersff7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharactersFf7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
