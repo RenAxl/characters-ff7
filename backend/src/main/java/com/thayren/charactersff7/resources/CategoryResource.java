121b9d3eeaa4f6040cc55ce94de56bf476cb8baa
Renan Duarte <renandg.axl@gmail.com>
2023-04-04 21:06:17 -0300

    Backend: Validation in insert/update of categories.

--- backend/src/main/java/com/thayren/charactersff7/resources/CategoryResource.java
+++ backend/src/main/java/com/thayren/charactersff7/resources/CategoryResource.java
@@ -2,6 +2,8 @@ package com.thayren.charactersff7.resources;
 
 import java.net.URI;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -53,7 +55,7 @@ public class CategoryResource {
 	}
 
 	@PostMapping
-	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+	public ResponseEntity<CategoryDTO> insert(@Valid @RequestBody CategoryDTO dto){
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
@@ -61,7 +63,7 @@ public class CategoryResource {
 	}
 
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto){
+	public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @Valid @RequestBody CategoryDTO dto){
 		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
