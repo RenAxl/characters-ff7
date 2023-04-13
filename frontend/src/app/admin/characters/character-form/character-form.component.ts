06dcca4a4723d9fdd96fc84acfece7a92e66fb40
Renan Duarte <renandg.axl@gmail.com>
2023-04-13 19:16:35 -0300

    Frontend: Character form page layout

--- frontend/src/app/admin/characters/character-form/character-form.component.ts
+++ frontend/src/app/admin/characters/character-form/character-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Character } from 'src/app/core/models/Character';
+
 
 @Component({
   selector: 'app-character-form',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterFormComponent implements OnInit {
 
+  character: Character = new Character();
+  
+  categories = [
+    { label: 'Principal', value: 1 },
+    { label: 'Membro da Shinra', value: 2 },
+    { label: 'Secundário', value: 3 },
+    { label: 'Vilão', value: 4 },
+    { label: 'Weapon', value: 5 },
+    { label: 'Turk', value: 6 },
+    { label: 'Monstro', value: 7 },
+    { label: 'Chefe', value: 8 },
+  ];
+
+  select: number[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  save(form: NgForm){
+    console.log(form.value);
+  }
+
 
 }
--- frontend/src/app/admin/characters/characters.module.ts
+++ frontend/src/app/admin/characters/characters.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 import { CharacterFormComponent } from './character-form/character-form.component';
 import { CharacterListComponent } from './character-list/character-list.component';
 import { CharactersRoutingModule } from './characters-routing.modules';
 import { AdminModule } from '../admin.module';
+import { CoreModule } from 'src/app/core/core.module';
 
 
 
@@ -19,11 +25,17 @@ import { AdminModule } from '../admin.module';
   ],
   imports: [
     CommonModule,
+    FormsModule,
 
     AdminModule,
     ButtonModule,
     TableModule,
     TooltipModule,
+    InputNumberModule,
+    InputTextModule,
+    InputTextareaModule,
+    DropdownModule,
+    CoreModule,
     CharactersRoutingModule
   ]
 })
